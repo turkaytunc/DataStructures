80576b317cfd7a2fda659da23282f804fcc08531
Turkay Tunc <turkaytunc00@gmail.com>
2019-05-19 14:05:38 +0300

    Update ShellSort

--- SortingAlgorithms/ShellSort/ShellSort/ShellSort.cpp
+++ SortingAlgorithms/ShellSort/ShellSort/ShellSort.cpp
@@ -1,11 +1,25 @@
 #include "pch.h"
 #include <iostream>
 
+using std::cout;
+using std::cin;
+using std::endl;
+
+void PrintArray(int *arr, int size);
 void ShellSort(int *intArray, int size);
 
 int main()
 {
-    
+	int A[11] = { 5,-1,0,3,7,1,7,0,21,34,6 };
+
+	PrintArray(A, 11);
+
+	ShellSort(A, 11);
+	PrintArray(A, 11);
+
+
+	char ch = ' ';
+	cin >> ch;
 }
 
 void ShellSort(int *intArray, int size)
@@ -38,4 +52,13 @@ void ShellSort(int *intArray, int size)
 		interval = (interval - 1) / 3;
 		i++;
 	}
+}
+
+void PrintArray(int *arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
 }
